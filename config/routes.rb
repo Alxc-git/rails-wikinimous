858b7d38ce6b6ee57316c6440b3c2097f8d65976
Alexis Giard <alexis.giard96@gmail.com>
2025-04-05 16:46:00 -0400

    Unfinished rails-wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # root "posts#index"
-  root 'index', to: 'articles#index'
+  root to: 'articles#index'
+  get 'articles', to: 'articles#index', as: :articles
+
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  get 'articles/update', to: 'articles#update'
+
+  delete 'articles/:id', to: 'articles#destroy'
+
+  get 'articles/new', to: 'articles#new', as: :new_article
+  post 'articles/new', to: 'articles#create'
+
+  get 'articles/:id', to: 'articles#show', as: :article
 end
