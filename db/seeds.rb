305fdb37506cce48e56f9eae128654b80ec5be66
Alexis Giard <alexis.giard96@gmail.com>
2025-04-05 15:35:02 -0400

    commit test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+10.times do
+  title = Faker::Lorem.sentence
+  content = Faker::Lorem.paragraph
+  article = Article.new(title: title, content: content)
+  article.save
+
+  puts "Article save! #{article.title}"
+end
